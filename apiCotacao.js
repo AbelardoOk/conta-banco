3f6d9c22234dbbc95ba49137a8db4e2639e7e4af
Abelardo Palácios <71283403+AbelardoOk@users.noreply.github.com>
2023-04-27 15:52:10 -0300

    Iplementada a api AlphaVantage

--- apiCotacao.js
+++ apiCotacao.js
@@ -1,2 +1,19 @@
-// Fazer uma parte de cotação aqui, em que você poderá ver
-// quantos dólares você tem com base no saldo bancário
+import axios from 'axios';
+
+export function cotacao(cnt){
+    const apiKey = 'IXFYDMK9H9VON85P';
+    const fromCurrency = 'BRL';
+    const toCurrency = 'USD';
+    const amount = cnt.saldo;
+
+    axios.get(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${fromCurrency}&to_currency=${toCurrency}&apikey=${apiKey}`)
+        .then(Response => {
+            const data = Response.data['Realtime Currency Exchange Rate'];
+            const exchangeRate = data['5. Exchange Rate'];
+            const convertedAmount = exchangeRate * amount;
+            console.log(`\n Você possuí ${convertedAmount.toFixed(2)} ${toCurrency} na conta.`);
+        }) 
+        .catch(error => {
+            console.error(error);
+        });  
+}
